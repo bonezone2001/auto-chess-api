9ce7603fe88c931622cd9be10bffc8f04c4d31a2
Bone <58440655+bonezone2001@users.noreply.github.com>
2021-03-19 21:04:26 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,9 @@
 -- Loader, check place ID and load correct script
 if game.PlaceId == 1268288957 then
+    print("Loading tuning chess script!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bonezone2001/AutoChessAPI/main/TuningChess.lua"))
 elseif game.PlaceId == 6222531507 then
+    print("Loading cookie chess script!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bonezone2001/AutoChessAPI/main/CookieChess.lua"))
 else
     error("Invalid game!")
